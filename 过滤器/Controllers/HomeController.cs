beeb1d22aeb9b18e85ff4743ad3897e24009d6ec
angular <1921934563@qq.com>
2019-07-01 17:10:30 +0800

    过滤器

---
+++ 过滤器/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 过滤器.Filter;
+
+namespace 过滤器.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Login(IsChecked =true)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string username,string pwd)
+        {
+            if (username == "123" && pwd == "123")
+            {
+                Session["username"] = "123";
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewBag.msg = "密码错误！";
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 过滤器/Filter/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 过滤器.Filter
+{
+    public class Login:ActionFilterAttribute
+    {
+        public bool IsChecked { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            if (IsChecked)
+            {
+                if (filterContext.HttpContext.Session["username"] == null)
+                {
+                    filterContext.HttpContext.Response.Redirect("/Home/Login");
+                }
+            }
+        }
+    }
+}
