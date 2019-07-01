4aab9cf71a9931cb5f5fb46b1c801bb495f4f82e
angular <1921934563@qq.com>
2019-07-01 15:13:46 +0800

    添加项目文件。

---
+++ 控制器和路由/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 控制器和路由.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            HttpCookie cookie = Request.Cookies["theme"]??new HttpCookie("theme","default");
+            ViewBag.theme = cookie.Value;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string theme)
+        {
+            HttpCookie cookie = new HttpCookie("theme",theme);
+            cookie.Expires = DateTime.MaxValue;
+            Response.SetCookie(cookie);
+            ViewBag.theme = cookie.Value;
+            return View();
+        }
+
+        
+        public ActionResult Css()
+        {
+            HttpCookie cookie = Request.Cookies["theme"]??new HttpCookie("theme","default");
+
+            switch (cookie.Value)
+            {
+                case "theme1":
+
+                    return Content("body{font-family:SimHei;font-size:1.2em;color:red;}","text/css");
+
+                case "theme2":
+
+                    return Content("body{font-family:KaiTi;font-size:2.2em;color:green;}", "text/css");
+
+                default:
+
+                    return Content("body{font-family:SimSong;font-size:3.2em;color:blue;}", "text/css");
+            }
+        }
+
+        public ActionResult Image(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return View();
+            string path = Server.MapPath("/Images/" + id+".png");
+            FileResult file= File(path, "image/png");
+            return file;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 控制器和路由/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="控制器和路由.MvcApplication" Language="C#" %>
