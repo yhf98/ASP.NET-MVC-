54c98288596edcff9769fd6a6558d144c3850122
angular <1921934563@qq.com>
2019-07-01 15:37:20 +0800

    创建一个BBS类

--- CopyAs/BBS.cs
+++ CopyAs/BBS.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CopyAs
 {
-    class Program
+    class BBS
     {
-        static void Main(string[] args)
-        {
-            
-        }
     }
 }
