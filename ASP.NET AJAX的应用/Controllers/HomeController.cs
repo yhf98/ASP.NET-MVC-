665c86ec5acabef1fdfa487a3f602ef4f2b557bc
angular <1921934563@qq.com>
2019-07-03 18:48:45 +0800

    2019-7-3

--- ASP.NET AJAX的应用/Controllers/HomeController.cs
+++ ASP.NET AJAX的应用/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP.NET_AJAX的应用.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,28 @@ namespace ASP.NET_AJAX的应用.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Search(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return View();
+            }
+            using (meixinEntities db = new meixinEntities())
+            {
+                var goods = db.m_product.ToList();
+                var result = goods.FindAll(F => F.pname.Contains(""));
+                return PartialView("_source", result);
+            }
+        }
+
+        public ActionResult Go()
+        {
+
+            ViewBag.Items = new string [] {"王二狗","WANNNG", "王二狗", "WANNNG", "王二狗", "WANNNG" };
+
+            return PartialView("_data");
         }
 
         public ActionResult About()
